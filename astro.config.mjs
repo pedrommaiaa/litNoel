197bbe60ec595b7808436a6ba24142358d288b3d
Pedro Maia <pedrommaiaa@protonmail.com>
2025-09-06 15:07:57 -0400

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,14 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), sitemap()],
+  integrations: [
+    tailwind(), 
+    sitemap({
+      changefreq: 'weekly',
+      priority: 0.7,
+      lastmod: new Date()
+    })
+  ],
   output: 'static',
   site: 'https://litnoel.com', // Your custom domain
   // base: '/website', // CUSTOMIZE: Uncomment and set to your repo name if not using custom domain
