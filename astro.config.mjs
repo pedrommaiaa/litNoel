7a4ab43c041fac485c94efa77c6530b1e8cd2435
Pedro Maia <pedrommaiaa@protonmail.com>
2025-09-06 15:00:59 -0400

    clean up

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [tailwind(), sitemap()],
   output: 'static',
   site: 'https://litnoel.com', // Your custom domain
   // base: '/website', // CUSTOMIZE: Uncomment and set to your repo name if not using custom domain
